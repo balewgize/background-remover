37354a1e7756bcd757366e7f1a32e72cf3df973c
Alemnew Marie <alemnewmarie461@gmail.com>
2023-03-24 23:21:58 +0300

    Refactoring and using latest version of dependencies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,4 @@
-# -*- coding: utf-8 -*-
-
-import imghdr
 import io
-import json
-import os
 import tempfile
 import time
 import uuid
@@ -11,15 +6,19 @@ import zipfile
 from pathlib import Path
 
 import streamlit as st
-from PIL import Image, ImageSequence
-from dotenv import load_dotenv
+from PIL import Image
 
-# from gotipy import Gotify
-from loguru import logger
-from rembg.bg import remove
+# from loguru import logger
+from rembg import remove
+
+
+MAX_FILES = 5  # number of images to be processed once
+MULTIPLE_IMAGES_ALLOWED = True
+ALLOWED_TYPES = ["png", "jpg", "jpeg"]
 
 
 def remove_bg(input_data, path):
+    """Remove background from an image using rembg."""
     result = remove(input_data)
     img = Image.open(io.BytesIO(result)).convert("RGBA")
     if Path(path).suffix != ".png":
@@ -27,183 +26,155 @@ def remove_bg(input_data, path):
     return img
 
 
-def gif2frames(input_file, skip_every=1):
-    im = Image.open(input_file)
-    include_frames = range(0, len(list(ImageSequence.Iterator(im))), skip_every)
+def build_ui():
+    """Show UI of the app with file upload form"""
+    st.markdown("### Image Backgroud Remover")
+    st.markdown("Remove background from images using pre-trained ML model.")
+    st.markdown("\n")
 
-    frames = []
+    if st.sidebar.button("CLEAR"):
+        st.session_state["key"] = session_id
+        st.experimental_rerun()
 
-    for n, frame in enumerate(ImageSequence.Iterator(im)):
-        if n not in include_frames:
-            continue
-        frame.copy()
-        bytes_obj = io.BytesIO()
-        frame.save(bytes_obj, format="PNG")
-        frames.append((n, bytes_obj.getvalue()))
-    return frames
+    st.sidebar.markdown("---")
+
+    uploaded_files = st.sidebar.file_uploader(
+        f"Choose one or multiple images (max: {MAX_FILES})",
+        type=ALLOWED_TYPES,
+        accept_multiple_files=MULTIPLE_IMAGES_ALLOWED,
+        key=st.session_state["key"],
+    )
+    return uploaded_files
+
+
+def get_image_bytes(uploaded_files):
+    """Return bytes data for each uploaded file."""
+    img_bytes = []
+    for uploaded_file in uploaded_files:
+        bytes_data = uploaded_file.getvalue()
+        if "btn" not in st.session_state:
+            # st.session_state.my_button = True
+            img_bytes.append((uploaded_file, bytes_data))
+
+    return img_bytes
 
 
 def main():
-    if st.sidebar.button("CLEAR"):
-        st.session_state["key"] = K
-        st.experimental_rerun()
-    st.sidebar.markdown("---")
+    uploaded_files = build_ui()
 
-    accept_multiple_files = True
-    accepted_type = ["png", "jpg", "jpeg", "gif"]
-
-    uploaded_files = st.sidebar.file_uploader(
-        f"Choose one or multiple files (max: {MAX_FILES})",
-        type=accepted_type,
-        accept_multiple_files=accept_multiple_files,
-        key=st.session_state["key"],
-    )
-
-    if len(uploaded_files) > MAX_FILES != -1:
+    if len(uploaded_files) > MAX_FILES:
         st.warning(
-            f"Maximum number of files reached! Only the first {MAX_FILES} "
-            "will be processed."
+            f"Maximum number of images reached! Only the first {MAX_FILES} will be processed."
         )
         uploaded_files = uploaded_files[:MAX_FILES]
 
-    uploaded_files = [x for x in uploaded_files if x]
+    # uploaded_files = [img for img in uploaded_files if img]
+    if not uploaded_files:
+        return
 
-    if uploaded_files:
-        logger.info(f"Uploaded the following files: {uploaded_files}")
+    # logger.info(f"Uploaded the following files: {uploaded_files}")
 
-        progress_bar = st.empty()
-        down_btn = st.empty()
-        cols = st.empty()
-        col1, col2 = cols.columns(2)
-        imgs_bytes = []
-        frames = []
+    progress_bar = st.empty()
+    download_btn = st.empty()
 
-        IS_GIF = False
-        if any(Path(x.name).suffix.lower() == ".gif" for x in uploaded_files):
-            IS_GIF = True
-            if len(uploaded_files) > 1:
-                st.error(
-                    f"The maximum number of allowed uploads when processing a "
-                    "GIF is one file!"
-                )
-                return
+    image_row = st.empty()  # row to hold original and result image
+    original, result = image_row.columns(2)
+    st.text("\n")
 
-            dur_text = "Duration (in milliseconds) of each frame:"
-            duration = st.sidebar.slider(dur_text, 0, 1000, 100, 10)
-            frames = gif2frames(uploaded_files[0])
+    # show original images
+    img_bytes = get_image_bytes(uploaded_files)
+    for b in img_bytes:
+        original.image(b[1], caption="Original")
 
-        for uploaded_file in uploaded_files:
-            bytes_data = uploaded_file.getvalue()
+    if not st.sidebar.button("Remove Background"):
+        return
 
-            if imghdr.what(file="", h=bytes_data) not in accepted_type:
-                st.error(f"`{uploaded_file.name}` is not a valid image!")
-                continue
+    nobg_images = []  # result images with no background
 
-            if "btn" not in st.session_state:
-                st.session_state.my_button = True
-                imgs_bytes.append((uploaded_file, bytes_data))
+    progress_text = "Operation in progress. Please wait."
+    progress = progress_bar.progress(0, text=progress_text)
 
-        col1.image([x[1] for x in imgs_bytes])
-
-        nobg_imgs = []
-        if st.sidebar.button("Remove background"):
-            pb = progress_bar.progress(0)
-
-            if frames:
-                imgs_bytes = frames
-
-            with st.spinner("Wait for it..."):
-                for n, (uploaded_file, bytes_data) in enumerate(imgs_bytes, start=1):
-                    if isinstance(uploaded_file, int):
-                        p = Path(str(uploaded_file) + ".png")
-                    else:
-                        p = Path(uploaded_file.name)
-
-                    img = remove_bg(bytes_data, p)
-                    with io.BytesIO() as f:
-                        img.save(f, format="PNG", quality=100, subsampling=0)
-                        data = f.getvalue()
-                    nobg_imgs.append((img, p, data))
-
-                    cur_progress = int(100 / len(imgs_bytes))
-                    pb.progress(cur_progress * n)
-                time.sleep(1)
-                progress_bar.empty()
-                pb.success("Complete!")
-
-                nobg_images = [x[0] for x in nobg_imgs]
-
-                if IS_GIF:
-                    col2.markdown(
-                        "🧪 *Use [ezgif.com](https://ezgif.com/) to create "
-                        "the GIF file and edit individual frames.*"
-                    )
-                col2.image(nobg_images)
-
-            if len(nobg_imgs) > 1:
-                with io.BytesIO() as tmp_zip:
-                    with zipfile.ZipFile(tmp_zip, "w") as z:
-                        for img, p, data in nobg_imgs:
-                            with tempfile.NamedTemporaryFile() as fp:
-                                img.save(fp.name, format="PNG")
-                                z.write(
-                                    fp.name,
-                                    arcname=p.name,
-                                    compress_type=zipfile.ZIP_DEFLATED,
-                                )
-                    zip_data = tmp_zip.getvalue()
-
-                if IS_GIF:
-                    frames_literal = "(individual frames)"
-                else:
-                    frames_literal = ""
-
-                down_btn.download_button(
-                    label=f"Download all results {frames_literal}",
-                    data=zip_data,
-                    file_name=f"results_{int(time.time())}.zip",
-                    mime="application/zip",
-                    key="btn",
-                )
+    with st.spinner("Processing image..."):
+        for i, image_byte in enumerate(img_bytes, start=1):
+            uploaded_file, bytes_data = image_byte
+            if isinstance(uploaded_file, int):
+                img_path = Path(str(uploaded_file) + ".png")
             else:
-                try:
-                    out = nobg_imgs[0]
-                    down_btn.download_button(
-                        label="Download result",
-                        data=out[-1],
-                        file_name=f"{out[1].stem}_nobg.png",
-                        mime="image/png",
-                        key="btn",
-                    )
-                except IndexError:
-                    st.error("No more images to process!")
-                finally:
-                    st.session_state["key"] = K
+                img_path = Path(uploaded_file.name)
+
+            img = remove_bg(bytes_data, img_path)
+            with io.BytesIO() as f:
+                img.save(f, format="PNG", quality=100, subsampling=0)
+                data = f.getvalue()
+
+            nobg_images.append((img, img_path, data))
+
+            cur_progress = int(100 / len(img_bytes))
+            progress.progress(cur_progress * i)
+
+        time.sleep(1)
+        progress_bar.empty()
+        progress.success("Complete!")
+
+        # show result image along side original image
+        for res in nobg_images:
+            result.image(res[0], caption="Result")
+
+    # multiple results will be downloaded as zip file
+    if len(nobg_images) > 1:
+        with io.BytesIO() as tmp_zip:
+            with zipfile.ZipFile(tmp_zip, "w") as z:
+                for img, path, data in nobg_images:
+                    with tempfile.NamedTemporaryFile() as fp:
+                        img.save(fp.name, format="PNG")
+                        z.write(
+                            fp.name,
+                            arcname=path.name,
+                            compress_type=zipfile.ZIP_DEFLATED,
+                        )
+            zip_data = tmp_zip.getvalue()
+
+        download_btn.download_button(
+            label=f"Download as ZIP",
+            data=zip_data,
+            file_name=f"results_{int(time.time())}.zip",
+            mime="application/zip",
+            key="btn",
+        )
+    else:
+        try:
+            output = nobg_images[0]
+            download_btn.download_button(
+                label="Download Result",
+                data=output[-1],
+                file_name=f"{output[1].stem}_nobg.png",
+                mime="image/png",
+                key="btn",
+            )
+        except IndexError:
+            st.error("No more images to process!")
+        finally:
+            st.session_state["key"] = session_id
+
+    time.sleep(3)
+    progress_bar.empty()
 
 
 if __name__ == "__main__":
     st.set_page_config(
-        page_title="Remove Background", page_icon="✂️", initial_sidebar_state="expanded"
+        page_title="Background Remover",
+        page_icon="✂️",
+        initial_sidebar_state="expanded",
     )
     st.markdown(
         "<style> footer {visibility: hidden;}"
         "#MainMenu {visibility: hidden;}</style>",
         unsafe_allow_html=True,
     )
-    logger.add("logs.log")
+    # logger.add("logs.log")
 
-    load_dotenv()
-
-    MAX_FILES = 10
-    if os.getenv("MAX_FILES"):
-        MAX_FILES = int(os.getenv("MAX_FILES"))
-
-    GOTIFY = False
-    if os.getenv("GOTIFY_HOST_ADDRESS") and os.getenv("GOTIFY_APP_TOKEN"):
-        GOTIFY = True
-
-    K = str(uuid.uuid4())
+    session_id = str(uuid.uuid4())
     if "key" not in st.session_state:
-        st.session_state["key"] = K
+        st.session_state["key"] = session_id
 
     main()
